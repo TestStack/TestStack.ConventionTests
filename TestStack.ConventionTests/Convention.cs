12d5fcb1d90157c06124d3f484727bd9a45003c3
Krzysztof Kozmic <krzysztof.kozmic@gmail.com>
2013-08-10 11:57:08 +1000

    some refactoring as warm up
    moving common logic into a separate method
    using Environment.NewLine to make Mono crowd happy

--- TestStack.ConventionTests/Convention.cs
+++ TestStack.ConventionTests/Convention.cs
@@ -1,35 +1,50 @@
 ﻿namespace TestStack.ConventionTests
 {
+    using System;
     using System.Diagnostics;
     using ApprovalTests;
     using ApprovalTests.Core.Exceptions;
+    using TestStack.ConventionTests.Internal;
 
     public static class Convention
     {
         public static void Is<TData>(IConvention<TData> convention, TData data) where TData : IConventionData
         {
-            data.EnsureHasNonEmptySource();
-            var result = convention.Execute(data);
+            var result = Execute(convention, data);
 
             if (result.Failed)
                 throw new ConventionFailedException(result.Message);
         }
 
-        public static void IsWithApprovedExeptions<TData>(IConvention<TData> convention, TData data) where TData : IConventionData
+        static ConventionResult Execute<TData>(IConvention<TData> convention, TData data) where TData : IConventionData
         {
             data.EnsureHasNonEmptySource();
             var result = convention.Execute(data);
+            return result;
+        }
+
+        public static void IsWithApprovedExeptions<TData>(IConvention<TData> convention, TData data)
+            where TData : IConventionData
+        {
+            var result = Execute(convention, data);
 
             // should we encapsulate Approvals behind Settings?
             try
             {
                 Approvals.Verify(result.Message);
 
-                Trace.WriteLine(string.Format("{0} has approved exceptions:\r\n\r\n{1}", convention.GetType().Name, result.Message));
+                Trace.WriteLine(string.Format("{0} has approved exceptions:{2}{2}{1}",
+                    convention.GetType().Name,
+                    result.Message,
+                    Environment.NewLine));
             }
             catch (ApprovalException ex)
             {
-                throw new ConventionFailedException("Approved exceptions for convention differs\r\n\r\n"+ex.Message, ex);
+                throw new ConventionFailedException(
+                    "Approved exceptions for convention differs" +
+                    Environment.NewLine +
+                    Environment.NewLine +
+                    ex.Message, ex);
             }
         }
     }
