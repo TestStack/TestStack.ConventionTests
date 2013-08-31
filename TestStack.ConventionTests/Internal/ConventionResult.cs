d9d8abeb0ea7c3c6b0dd9853fd7695e2466935de
Jake Ginnivan <jake.ginnivan@readify.net>
2013-08-31 10:56:40 +0100

    Added equality and formatting members to ConventionResult

--- TestStack.ConventionTests/Internal/ConventionResult.cs
+++ TestStack.ConventionTests/Internal/ConventionResult.cs
@@ -31,5 +31,44 @@
             FormattedResult = formattedResult;
             RecommendedFileExtension = recommendedFileExtension;
         }
+
+        protected bool Equals(ConventionResult other)
+        {
+            return DataType == other.DataType && string.Equals(ConventionTitle, other.ConventionTitle) && string.Equals(DataDescription, other.DataDescription);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ConventionResult) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (DataType != null ? DataType.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (ConventionTitle != null ? ConventionTitle.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (DataDescription != null ? DataDescription.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(ConventionResult left, ConventionResult right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ConventionResult left, ConventionResult right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("FormattedResult: {0}, DataDescription: {1}, ConventionTitle: {2}, DataType: {3}, HasData: {4}, RecommendedFileExtension: {5}", FormattedResult, DataDescription, ConventionTitle, DataType, HasData, RecommendedFileExtension);
+        }
     }
 }
