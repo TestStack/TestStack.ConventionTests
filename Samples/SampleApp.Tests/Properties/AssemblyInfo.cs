f38792c480518a045b51908d1b2a26fc90b4a4da
Jason Finch <jason.finch@gmail.com>
2020-03-29 10:48:28 +1000

    chore (Assembly): Update copyright to 2020

--- Samples/SampleApp.Tests/Properties/AssemblyInfo.cs
+++ Samples/SampleApp.Tests/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SampleApp.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
